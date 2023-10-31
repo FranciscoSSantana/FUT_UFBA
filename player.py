9ef2022028687df264f4dfb49fe42cc171d3da01
FranciscoSSantana <148816700+FranciscoSSantana@users.noreply.github.com>
2023-10-30 22:23:48 -0300

    Update player.py

--- player.py
+++ player.py
@@ -1,12 +1,45 @@
 from tupy import *
 
 class Player(Image):
-    def __init__(self, x = 100, y = 100):
+
+    def __init__(self, x = 100, y = 400, file = "player.jpg", wasd_scheme = True):
         self.x = x
         self.y = y
+        self.jumping = False
+        self.file = file
+
+        self.x_speed = 5
+        self.jump_height = 20
+        self.y_speed = self.jump_height
+        self.y_gravity = 2
+
+        if wasd_scheme:
+            self.left = 'a'
+            self.right = 'd'
+            self.jump_key = 'w'
+            self.power_key = 'space'
+        else:
+            self.left = 'Left'
+            self.right = 'Right'
+            self.jump_key = 'Up'
+            self.power_key = 'enter'
 
     def kick(self):
         pass
 
     def update(self):
-        if keyboard.is_key_just_down('a'):
+        if keyboard.is_key_down(self.left):
+            self.x -= self.x_speed
+        if keyboard.is_key_down(self.right):
+            self.x += self.x_speed
+        if keyboard.is_key_just_down(self.jump_key):
+            self.jumping = True
+        if keyboard.is_key_down(self.power_key):
+            pass
+
+        if self.jumping:
+            self.y -= self.y_speed
+            self.y_speed -= self.y_gravity
+            if self.y_speed < -self.jump_height:
+                self.jumping = False
+                self.y_speed = self.jump_height
