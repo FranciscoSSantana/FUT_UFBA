0f522faa08720917a4a205dcf495e4e3ce05db81
FranciscoSSantana <148816700+FranciscoSSantana@users.noreply.github.com>
2023-11-02 15:26:01 -0300

    Update player.py

--- player.py
+++ player.py
@@ -1,8 +1,16 @@
 from tupy import *
 
+RIGHT_BOUNDARY = 850
+LEFT_BOUNDARY = 50
+TOP_BOUNDARY = 0
+BOTTOM_BOUNDARY = 500
+GROUND_BOUNDARY = 460
+PLAYER_HEIGHT = 64
+PLAYER_SPAWN = 150
+
 class Player(Image):
 
-    def __init__(self, x, y, file, wasd_scheme=True):
+    def __init__(self, x: int = PLAYER_SPAWN, y : int = GROUND_BOUNDARY - PLAYER_HEIGHT/2, file: str = "Player Blue.png", wasd_scheme: bool = True) -> None:
         self.x = x
         self.y = y
         self.jumping = False
@@ -11,7 +19,7 @@ class Player(Image):
         self.x_speed = 5
         self.jump_height = 20
         self.y_speed = self.jump_height
-        self.y_gravity = 2
+        self.gravity = 2
         self.mass = 2
 
         if wasd_scheme:
@@ -40,7 +48,7 @@ class Player(Image):
 
         if self.jumping:
             self.y -= self.y_speed
-            self.y_speed -= self.y_gravity
+            self.y_speed -= self.gravity
             if self.y_speed < -self.jump_height:
                 self.jumping = False
                 self.y_speed = self.jump_height
