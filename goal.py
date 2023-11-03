d3fae0b6aff5b8656e8c49addf81c710d04994a0
FranciscoSSantana <148816700+FranciscoSSantana@users.noreply.github.com>
2023-11-03 15:22:43 -0300

    aesthetic changes

--- goal.py
+++ goal.py
@@ -1,7 +1,11 @@
 from tupy import *
 
+RIGHT_GOAL_X = 830
+LEFT_GOAL_X = 70
+GOAL_Y = 385
+
 class Goal(Image):
-    def __init__(self, x = 70, y = 385, file = "Goal Front Esq.png"):
+    def __init__(self, x = LEFT_GOAL_X, file = "Goal Front Esq.png"):
         self.x = x
-        self.y = y
+        self.y = GOAL_Y
         self.file = file
