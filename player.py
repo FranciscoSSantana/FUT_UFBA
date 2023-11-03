802cc2905df07f47c0e91fe2e80aca76c04b01b9
FranciscoSSantana <148816700+FranciscoSSantana@users.noreply.github.com>
2023-11-03 12:41:33 -0300

    Update player.py

--- player.py
+++ player.py
@@ -6,22 +6,22 @@ TOP_BOUNDARY = 0
 BOTTOM_BOUNDARY = 500
 GROUND_BOUNDARY = 460
 PLAYER_HEIGHT = 64
-PLAYER_SPAWN = 150
+PLAYER_SPAWN = 250
 
 class Player(Image):
-    PLAYERS = []
 
-    def __init__(self, x: int = PLAYER_SPAWN, y : int = GROUND_BOUNDARY - PLAYER_HEIGHT/2, file: str = 'Player Blue.png', wasd_scheme: bool = True) -> None:
+    def __init__(self, x: int = PLAYER_SPAWN, y : int = GROUND_BOUNDARY - PLAYER_HEIGHT/2, file: str = "Player Blue.png", wasd_scheme: bool = True) -> None:
         self.x = x
         self.y = y
         self.jumping = False
         self.file = file
 
-        self.x_speed = 5
+        self.x_speed = 0
         self.jump_height = 20
-        self.y_speed = self.jump_height
+        self.y_speed = 0
         self.gravity = 2
         self.mass = 2
+        self.radius = 32
 
         if wasd_scheme:
             self.left = 'a'
@@ -33,25 +33,31 @@ class Player(Image):
             self.right = 'Right'
             self.jump_key = 'Up'
             self.power_key = 'enter'
-        
-        self.PLAYERS.append(self)
 
     def kick(self):
         pass
 
     def update(self):
-        if keyboard.is_key_down(self.left) and self.x > LEFT_BOUNDARY:
-            self.x -= self.x_speed
-        if keyboard.is_key_down(self.right) and self.x < RIGHT_BOUNDARY:
-            self.x += self.x_speed
-        if keyboard.is_key_just_down(self.jump_key):
+        if keyboard.is_key_down(self.left):
+            self.x_speed = -5
+        if keyboard.is_key_down(self.right):
+            self.x_speed = 5
+        if keyboard.is_key_just_down(self.jump_key) and self.jumping is False:
             self.jumping = True
+            self.y_speed = self.jump_height
         if keyboard.is_key_down(self.power_key):
             pass
+        
+        self.x += self.x_speed
 
         if self.jumping:
             self.y -= self.y_speed
             self.y_speed -= self.gravity
             if self.y_speed < -self.jump_height:
                 self.jumping = False
-                self.y_speed = self.jump_height
+                self.y_speed = 0
+        
+        if keyboard.is_key_up(self.left) and self.x_speed < 0:
+            self.x_speed = 0
+        if keyboard.is_key_up(self.right) and self.x_speed > 0:
+            self.x_speed = 0
