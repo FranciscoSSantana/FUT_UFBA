c1f366e7c93c282a48796c1758d8ed06d6d8367f
FranciscoSSantana <148816700+FranciscoSSantana@users.noreply.github.com>
2023-11-04 03:29:31 -0300

    gameconstants model

--- background.py
+++ background.py
@@ -1,10 +1,8 @@
 from tupy import *
-
-CENTER_X = 450
-CENTER_Y = 250
+from gameconstants import *
 
 class Background(Image):
     def __init__(self, file = "CampJapan.png"):
-        self.x = CENTER_X
-        self.y = CENTER_Y
+        self.x = X_CENTER
+        self.y = Y_CENTER
         self.file = file
