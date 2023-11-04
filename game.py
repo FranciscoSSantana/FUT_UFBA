f564f4723d528abf6e3e531a18c94c4e5487d7f9
FranciscoSSantana <148816700+FranciscoSSantana@users.noreply.github.com>
2023-11-04 03:26:32 -0300

    small logical fixes

--- game.py
+++ game.py
@@ -1,14 +1,14 @@
 from tupy import *
-from background import *
-from player import *
-from ball import *
-from goal import *
+from background import Background
+from player import Player
+from ball import Ball
+from goal import Goal
 
 bg = Background()
+playerBlue = Player(True)
+playerRed = Player(False)
 bola = Ball()
-gl = Goal()
-gl2 = Goal(x=830, file= "Goal Front Dir.png")
-playerBlue = Player(x = 450)
-playerRed = Player(750, 428, "Player Red.png", False)
+gl = Goal(True)
+gl2 = Goal(False)
 
 run(globals())
