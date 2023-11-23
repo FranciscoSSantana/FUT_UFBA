baca73f2a765e9c2f11952ace3076269bfe58c38
FranciscoSSantana <148816700+FranciscoSSantana@users.noreply.github.com>
2023-11-23 03:32:28 -0300

    mudança total, game agora só roda o menu principal

--- game.py
+++ game.py
@@ -1,19 +1,6 @@
 from tupy import *
-from player import Player
-from ball import Ball
-from goal import Goal
-from background import Background
-from pause import Pause
-from placar import Placar
+from mainMenu import MainMenu
 
-bg = Background()
-pause = Pause()
-playerBlue = Player(True)
-playerRed = Player(False)
-placar = Placar()
-bola = Ball(placar)
-gl = Goal(True)
-gl2 = Goal(False)
+game = MainMenu()
 
 run(globals())
-
