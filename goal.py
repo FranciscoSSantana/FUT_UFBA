36a4cf9f845d82c4f1ac7680f83d4c8450e9ecd7
FranciscoSSantana <148816700+FranciscoSSantana@users.noreply.github.com>
2023-11-26 21:48:00 -0300

    Update goal.py

--- goal.py
+++ goal.py
@@ -1,12 +1,9 @@
 from tupy import *
 from gameconstants import *
 
-class Goal(Image):
-    def __init__(self, leftSide: bool = True):
-        self.y = GOAL_Y
-        if leftSide:
-            self.x = LEFT_GOAL_X
-            self.file = "Goal Front Esq.png"
-        else:
-            self.x = RIGHT_GOAL_X
-            self.file = "Goal Front Dir.png"
+class Goal(BaseImage):
+    def __init__(self, leftSide: bool = True) -> None:
+        self._y: int = GOAL_Y
+        
+        self._x: int = LEFT_GOAL_X if leftSide else RIGHT_GOAL_X
+        self._file: str = "GoalFrontEsq.png" if leftSide else "GoalFrontDir.png"
