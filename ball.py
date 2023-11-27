6b2090a551c06223b15627f944dc24c5a5705f51
FranciscoSSantana <148816700+FranciscoSSantana@users.noreply.github.com>
2023-11-26 21:46:44 -0300

    Update ball.py

--- ball.py
+++ ball.py
@@ -4,67 +4,85 @@ from player import Player
 from pause import Pause
 import numpy as np
 
-class Ball(Image):
-    def __init__(self, placar):
-        self.radius = BALL_RADIUS
-        self.stopBounce = BALL_BOUNCE_STOP_COEFFICIENT
-        self.elasticity = BALL_ELASTICITY
-        self.frictionCoefficient = BALL_FRICTION_COEFFICIENT
-        self.stopMovement = BALL_STOP_MOVEMENT_COEFFICIENT
-        self.placar = placar
+class Ball(BaseImage):
+    def __init__(self) -> None:
+        self._radius: int = BALL_RADIUS
+        self._stopBounce: float = BALL_BOUNCE_STOP_COEFFICIENT
+        self._elasticity: float = BALL_ELASTICITY
+        self._frictionCoefficient: float = BALL_FRICTION_COEFFICIENT
+        self._stopMovement: float = BALL_STOP_MOVEMENT_COEFFICIENT
 
-        self.gravity = BALL_GRAVITY
-        self.mass = BALL_MASS
+        self._gravity: float = BALL_GRAVITY
+        self._mass: float = BALL_MASS
 
         self.kick_off()
     
-    def kick_off(self):
-        self.x = BALL_KICK_OFF_X
-        self.y = BALL_KICK_OFF_Y
-        self.x_speed = BALL_KICK_OFF_X_SPEED
-        self.y_speed = BALL_KICK_OFF_Y_SPEED
-        for player in Player.PLAYERS:
+    def kick_off(self) -> None:
+        '''
+        Standard start/restart function. Also used upon goal detection
+        '''
+        self._x: int = BALL_KICK_OFF_X
+        self._y: int = BALL_KICK_OFF_Y
+        self._x_speed: float = BALL_KICK_OFF_X_SPEED
+        self._y_speed: float = BALL_KICK_OFF_Y_SPEED
+
+        playerList: list[Player] = Player.playerList()
+        for player in playerList:
             player.kick_off()
     
-    def blue_kick_off(self):
+    def _blue_kick_off(self) -> None:
+        '''
+        kick-off after Blue Player received a goal
+        '''
         self.kick_off()
-        self.x_speed = BALL_BLUE_KICK_OFF_X_SPEED
+        self._x_speed = BALL_BLUE_KICK_OFF_X_SPEED
     
-    def red_kick_off(self):
+    def _red_kick_off(self) -> None:
+        '''
+        kick-off after Red Player received a goal
+        '''
         self.kick_off()
-        self.x_speed = BALL_RED_KICK_OFF_X_SPEED
+        self._x_speed = BALL_RED_KICK_OFF_X_SPEED
     
-    def handleGravity_and_FloorCollision(self):
+    def _handleGravity_and_FloorCollision(self) -> None:
         #Apply gravity
-        if self.y < (GROUND_BOUNDARY - self.radius):
-            self.y_speed += self.gravity
+        if self._y < (GROUND_BOUNDARY - self._radius):
+            self._y_speed += self._gravity
         else:
             #Floor collision
-            if self.y_speed > self.stopBounce:
-                self.y_speed = self.y_speed * (-1) * self.elasticity
+            if self._y_speed > self._stopBounce:
+                self._y_speed = self._y_speed * (-1) * self._elasticity
             else:
-                if abs(self.y_speed) <= self.stopBounce:
-                    self.y_speed = 0
+                if abs(self._y_speed) <= self._stopBounce:
+                    self._y_speed = 0
     
-    def checkPlayerCollision(self):
-        if len(Player.PLAYERS) > 0:
-            for player in Player.PLAYERS:
+    def _checkPlayerCollision(self) -> None:
+        '''
+        Checks if a player and the ball have collided. Upon collision,
+        applies cooldown for colliding again.
+        '''
+        playerList: list[Player] = Player.playerList()
+        if len(playerList) > 0:
+            for player in playerList:
 
-                if player.ballCollisionCooldown < PLAYER_BALL_COLLISION_COOLDOWN:
-                    player.ballCollisionCooldown += 1
+                player.tickBallCollisionCooldown()
 
-                distance = ((self.x - player.x)**2 + (self.y - player.y)**2)**0.5
-                if (distance <= self.radius + player.radius) and (player.ballCollisionCooldown == PLAYER_BALL_COLLISION_COOLDOWN):
-                    player.ballCollisionCooldown = 0
-                    self.handlePlayerCollision(player)
+                distance = ((self._x - player.x)**2 + (self._y - player.y)**2)**0.5
+                if (distance <= self._radius + player.radius) and (player.ballCollisionCooldown == PLAYER_BALL_COLLISION_COOLDOWN):
+                    player.resetBallCollisionCooldown()
+                    self._handlePlayerCollision(player)
 
-    def handlePlayerCollision(self, player):
-        normal_vector = [self.x - player.x, self.y - player.y]
-        normal_vector = np.array(normal_vector)
+    def _handlePlayerCollision(self, player: Player) -> None:
+        '''
+        Calculates new velocities for the ball after collision
+        with player. Calculations made based on post by Chad Berchek, 2009
+        https://www.vobarian.com/collisions/2dcollisions2.pdf
+        '''
+        normal_vector = np.array([self._x - player.x, self._y - player.y])
         normal_vector = normal_vector / np.linalg.norm(normal_vector)
         tangent_vector = np.array([-1 * normal_vector[1], normal_vector[0]])
 
-        ball_speed_vector = np.array([self.x_speed, self.y_speed])
+        ball_speed_vector = np.array([self._x_speed, self._y_speed])
         player_speed_vector = np.array([player.x_speed, player.y_speed])
 
         ball_normal_speed = np.dot(normal_vector, ball_speed_vector)
@@ -72,84 +90,91 @@ class Ball(Image):
 
         player_normal_speed = np.dot(normal_vector, player_speed_vector)
 
-        ball_new_normal_speed = (ball_normal_speed * (self.mass - player.mass) + (2 * player.mass * player_normal_speed)) / (self.mass + player.mass)
+        ball_new_normal_speed = (ball_normal_speed * (self._mass - player.mass) + (2 * player.mass * player_normal_speed)) / (self._mass + player.mass)
         ball_speed_vector = (ball_new_normal_speed * normal_vector) + (ball_tangent_speed * tangent_vector)
 
-        self.x_speed = ball_speed_vector[0] * self.elasticity
-        self.y_speed = ball_speed_vector[1] * self.elasticity
+        self._x_speed = ball_speed_vector[0] * self._elasticity
+        self._y_speed = ball_speed_vector[1] * self._elasticity
 
-    def handleRoofCollision(self):
-        if ((self.y < UPPER_BOUNDARY + self.radius) and (self.y_speed < 0)):
-            self.y_speed = self.y_speed * (-1) * self.elasticity
+    def _handleRoofCollision(self) -> None:
+        if ((self._y < UPPER_BOUNDARY + self._radius) and (self._y_speed < 0)):
+            self._y_speed = self._y_speed * (-1) * self._elasticity
 
-    def handleLeftWallCollision_and_GoalDetection(self):
+    def _handleLeftWallCollision_and_GoalDetection(self) -> bool:
+        '''
+        Returns True if goal was detected, False if not.
+        '''
         #Side-wall collisions
-        if ((self.x < LEFT_BOUNDARY + self.radius) and (self.x_speed < 0)):
+        if ((self._x < LEFT_BOUNDARY + self._radius) and (self._x_speed < 0)):
             
-            self.x_speed = self.x_speed * (-1) * self.elasticity
+            self._x_speed = self._x_speed * (-1) * self._elasticity
 
             #Goal detection
-            if ((self.y < GROUND_BOUNDARY) and (self.y > GROUND_BOUNDARY - GOAL_SIZE)):
-                self.blue_kick_off()
-                self.placar.addRedGoal()
-        
+            if ((self._y < GROUND_BOUNDARY) and (self._y > GROUND_BOUNDARY - GOAL_SIZE)):
+                self._blue_kick_off()
+                return True
+        return False
 
-    def handleRightWallCollision_and_GoalDetection(self):
-            #Side-wall collisions
-            if ((self.x > RIGHT_BOUNDARY - self.radius) and (self.x_speed > 0)):
-                
-                self.x_speed = self.x_speed * (-1) * self.elasticity
+    def _handleRightWallCollision_and_GoalDetection(self) -> bool:
+        '''
+        Returns True if goal was detected, False if not.
+        '''
+        #Side-wall collisions
+        if ((self._x > RIGHT_BOUNDARY - self._radius) and (self._x_speed > 0)):
+            
+            self._x_speed = self._x_speed * (-1) * self._elasticity
 
-                #Goal detection
-                if ((self.y < GROUND_BOUNDARY) and (self.y > GROUND_BOUNDARY - GOAL_SIZE)):
-                    self.red_kick_off()
-                    self.placar.addBlueGoal()
+            #Goal detection
+            if ((self._y < GROUND_BOUNDARY) and (self._y > GROUND_BOUNDARY - GOAL_SIZE)):
+                self._red_kick_off()
+                return True
+        return False
     
-    def handleWallCollision_and_GoalDetection(self):
-        self.handleLeftWallCollision_and_GoalDetection()
-        self.handleRightWallCollision_and_GoalDetection()
-        self.handleRoofCollision()
+    def _handleWallCollision_and_GoalDetection(self) -> None:
+        self._handleLeftWallCollision_and_GoalDetection()
+        self._handleRightWallCollision_and_GoalDetection()
+        self._handleRoofCollision()
 
-    def handleGoalpostCollision(self):
+    def _handleGoalpostCollision(self) -> None:
 
         #top-side post collision
-        if (self.y + self.radius > GOALPOST_TOP) and \
-           (self.y < GOALPOST_TOP) and \
-           ((self.x + self.radius >= GOALPOST_RIGHT_WALL) or (self.x - self.radius <= GOALPOST_LEFT_WALL)):
+        if (self._y + self._radius > GOALPOST_TOP) and \
+           (self._y < GOALPOST_TOP) and \
+           ((self._x + self._radius >= GOALPOST_RIGHT_WALL) or (self._x - self._radius <= GOALPOST_LEFT_WALL)):
             
-            self.y_speed *= (-1) * self.elasticity
+            self._y_speed *= (-1) * self._elasticity
 
             #avoid cases of stuck ball
-            if (self.x >= RIGHT_BOUNDARY - GOALPOST_WIDTH):
-                self.x_speed -= 1
-            elif(self.x <= LEFT_BOUNDARY + GOALPOST_WIDTH):
-                self.x_speed += 1
+            if (self._x >= RIGHT_BOUNDARY - GOALPOST_WIDTH):
+                self._x_speed -= 1
+            elif(self._x <= LEFT_BOUNDARY + GOALPOST_WIDTH):
+                self._x_speed += 1
 
-    def handleFriction(self):
-        if self.y_speed == 0 and self.x_speed != 0.0:
-            if self.x_speed > 0:
-                self.x_speed -= self.frictionCoefficient
-            elif self.x_speed < 0:
-                self.x_speed += self.frictionCoefficient
-        if abs(self.x_speed) <= self.stopMovement:
-            self.x_speed = 0
+    def _handleFriction(self) -> None:
+        if self._y_speed == 0 and self._x_speed != 0.0:
+            if self._x_speed > 0:
+                self._x_speed -= self._frictionCoefficient
+            elif self._x_speed < 0:
+                self._x_speed += self._frictionCoefficient
+        if abs(self._x_speed) <= self._stopMovement:
+            self._x_speed = 0
     
-    def handleSpin(self):
-        if self.x_speed > 0:
-            self.angle -= BALL_SPIN
-        if self.x_speed < 0:
-            self.angle += BALL_SPIN
+    def _handleSpin(self) -> None:
+        if self._x_speed > 0:
+            self._angle -= BALL_SPIN
+        if self._x_speed < 0:
+            self._angle += BALL_SPIN
 
-    def handlePhysics(self):
-        self.handleGravity_and_FloorCollision()
-        self.handleWallCollision_and_GoalDetection()
-        self.handleFriction()
-        self.checkPlayerCollision()
-        self.handleSpin()
-        self.handleGoalpostCollision()
-        self.y += self.y_speed
-        self.x += self.x_speed
+    def _handlePhysics(self) -> None:
+        self._handleGravity_and_FloorCollision()
+        self._handleWallCollision_and_GoalDetection()
+        self._handleFriction()
+        self._checkPlayerCollision()
+        self._handleSpin()
+        self._handleGoalpostCollision()
+        self._y += round(self._y_speed)
+        self._x += round(self._x_speed)
 
-    def update(self):
-        if not Pause.isPaused:
-            self.handlePhysics()
+    def update(self) -> None:
+        if not Pause.isGamePaused():
+            self._handlePhysics()
