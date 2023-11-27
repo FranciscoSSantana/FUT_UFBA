0d29e9235386b8ecba5f96904f158653a8dcbe25
FranciscoSSantana <148816700+FranciscoSSantana@users.noreply.github.com>
2023-11-26 21:48:27 -0300

    Update pause.py

--- pause.py
+++ pause.py
@@ -2,67 +2,83 @@ from tupy import *
 from gameconstants import *
 from buttons import MenuBackground, Resume, Restart
 
-class Pause(Image):
-    _isPaused = False
+class Pause(BaseImage):
+    _isPaused: bool = False
 
-    @property
-    def isPaused(self):
+    @classmethod
+    def isGamePaused(cls) -> bool:
         return Pause._isPaused
-    
-    @isPaused.setter
-    def isPaused(self, p):
-        Pause._isPaused = p
 
-    def __init__(self):
-        self.x = RIGHT_BOUNDARY - BUTTON_OFFSET
-        self.y = UPPER_BOUNDARY + BUTTON_OFFSET
-        self.file = "pause.png"
-        self.gameRestart = False
-        self.menuBackground = MenuBackground()
-        self.resumeButton = Resume()
-        self.restartButton = Restart()
-        self.resume()
+    @classmethod
+    def freeze(cls) -> None:
+        Pause._isPaused = True
+
+    @classmethod
+    def unfreeze(cls) -> None:
+        Pause._isPaused = False
+
+    def __init__(self) -> None:
+        self._x: int = RIGHT_BOUNDARY - BUTTON_OFFSET
+        self._y: int = UPPER_BOUNDARY + BUTTON_OFFSET
+        self._file: str = "pause.png"
+        self._gameRestart: bool = False
+        self._menuBackground: MenuBackground = MenuBackground()
+        self._resumeButton: Resume = Resume()
+        self._restartButton: Restart = Restart()
+        self._resume()
     
     def _destroy(self) -> None:
-        self.menuBackground._destroy()
-        self.resumeButton._destroy()
-        self.restartButton._destroy()
+        self._menuBackground._destroy()
+        self._resumeButton._destroy()
+        self._restartButton._destroy()
         super()._destroy()
 
-    def kick_off(self):
-        self.resume()
-        self.gameRestart = False
+    def kick_off(self) -> None:
+        '''
+        Standard start/restart function.
+        '''
+        self._resume()
+        self._gameRestart = False
 
-    def pause(self):
-        self.resumeButton._show()
-        self.restartButton._show()
-        self.menuBackground._show()
-        Pause.isPaused = True
+    def _pause(self) -> None:
+        self._resumeButton._show()
+        self._restartButton._show()
+        self._menuBackground._show()
+        Pause.freeze()
     
-    def resume(self):
-        self.resumeButton._hide()
-        self.restartButton._hide()
-        self.menuBackground._hide()
-        Pause.isPaused = False
+    def _resume(self) -> None:
+        self._resumeButton._hide()
+        self._restartButton._hide()
+        self._menuBackground._hide()
+        Pause.unfreeze()
     
-    def restart(self):
-        self.gameRestart = True
-        self.resume()
+    def _restart(self) -> None:
+        self._gameRestart = True
+        self._resume()
 
-    def detectClick(self):
-        if Pause.isPaused:
-            if (self.resumeButton._contains_point(mouse.x, mouse.y) or self._contains_point(mouse.x, mouse.y)) \
-                and mouse.is_button_just_down():
-                self.resume()
+    def _detectPauseCommand(self) -> None:
+        '''
+        Handles pause buttons and actions.
+        '''
+        if Pause.isGamePaused():
+            if ((self._resumeButton._contains_point(mouse.x, mouse.y) or self._contains_point(mouse.x, mouse.y)) \
+                and mouse.is_button_just_down()) \
+                or keyboard.is_key_just_down('p'):
+                self._resume()
 
-            elif self.restartButton._contains_point(mouse.x, mouse.y) and mouse.is_button_just_down():
-                self.restart()
+            elif self._restartButton._contains_point(mouse.x, mouse.y) and mouse.is_button_just_down():
+                self._restart()
 
         else:
-            if self._contains_point(mouse.x, mouse.y) and mouse.is_button_just_down():
-                self.pause()
+            if (self._contains_point(mouse.x, mouse.y) and mouse.is_button_just_down()) \
+                or keyboard.is_key_just_down('p'):
 
-    def update(self):
-        self.detectClick()
-        
+                self._pause()
+
+    def update(self) -> None:
+        self._detectPauseCommand()
+    
+    @property
+    def gameRestart(self) -> bool:
+        return self._gameRestart
                 
